04bdab784d9010c8f81537c3a772a00a4b360ca0
Faraz Husain <fhusain@adaptready.com>
2022-09-09 14:38:20 +0530

    Included header to Home

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "../../Components/Header/Header";
 import NavBar from "../../Components/Navbar/NavBar";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <Header />
     </div>
   );
 };
